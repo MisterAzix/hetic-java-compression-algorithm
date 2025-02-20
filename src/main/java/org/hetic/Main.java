63cb7496625f5160cc6f19b65ae200dc1908893b
DFarau <44142390+DFarau@users.noreply.github.com>
2025-02-20 14:44:14 +0100

    chore: add performance test for chunking process (#25)
    * chore: add performance test for chunking process
    
    * chore: remove performance test file and move logic to Main
    
    * chore: use existing file to calculate processiong time instead of generation content

--- src/main/java/org/hetic/Main.java
+++ src/main/java/org/hetic/Main.java
@@ -6,7 +6,11 @@ import org.hetic.adapters.strategy.SHA256HashingStrategy;
 import org.hetic.domain.ChunkingService;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -21,7 +25,13 @@ public class Main {
             rabinChunkingStrategy,
             hashingStrategy
         );
+
+        Instant start = Instant.now();
         inMemoryService.processFile(file);
+        Instant end = Instant.now();
+
+        Duration timeElapsed = Duration.between(start, end);
+        System.out.println("Time taken: " + timeElapsed.toMillis() + " ms");
 
         inMemoryChunkRepository.getStorage().forEach(chunk -> System.out.println(chunk.length));
     }
