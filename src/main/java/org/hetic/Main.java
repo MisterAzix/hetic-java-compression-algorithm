85917399144c91aab0997a60d8e7a8a29fd6bce6
DFarau <doriane.farau@hetic.net>
2025-02-20 11:06:16 +0100

    chore: remove performance test file and move logic to Main

--- src/main/java/org/hetic/Main.java
+++ src/main/java/org/hetic/Main.java
@@ -6,11 +6,21 @@ import org.hetic.adapters.strategy.SHA256HashingStrategy;
 import org.hetic.domain.ChunkingService;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         File file = new File("src/main/resources/file.txt");
+
+        byte[] content = new byte[10240];
+        Arrays.fill(content, (byte) 1);
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            fos.write(content);
+        }
 
         InMemoryChunkRepository inMemoryChunkRepository = new InMemoryChunkRepository();
         RabinChunkingStrategy rabinChunkingStrategy = new RabinChunkingStrategy();
@@ -21,7 +31,13 @@ public class Main {
             rabinChunkingStrategy,
             hashingStrategy
         );
+
+        Instant start = Instant.now();
         inMemoryService.processFile(file);
+        Instant end = Instant.now();
+
+        Duration timeElapsed = Duration.between(start, end);
+        System.out.println("Time taken: " + timeElapsed.toMillis() + " ms");
 
         inMemoryChunkRepository.getStorage().forEach(chunk -> System.out.println(chunk.length));
     }
