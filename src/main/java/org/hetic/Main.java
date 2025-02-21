a3c825faa0a153ce2dd295091c56dc5079efe38a
MisterAzix <flizi@live.fr>
2025-02-21 13:43:53 +0100

    feat: adding logs 🔊

--- src/main/java/org/hetic/Main.java
+++ src/main/java/org/hetic/Main.java
@@ -4,7 +4,7 @@ import org.hetic.adapters.inMemory.InMemoryChunkRepository;
 import org.hetic.adapters.inMemory.InMemoryFileRepository;
 import org.hetic.adapters.rabin.RabinChunkingStrategy;
 import org.hetic.adapters.sha256.SHA256HashingStrategy;
-import org.hetic.adapters.zstd.ZstdCompressionStrategy;
+import org.hetic.config.AppConfig;
 import org.hetic.domain.ChunkingService;
 import org.hetic.domain.CompressionFactory;
 
@@ -12,9 +12,11 @@ import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        printSplashScreen();
         File file = new File("src/main/resources/test_image_duplication.png");
 
         InMemoryChunkRepository inMemoryChunkRepository = new InMemoryChunkRepository();
@@ -31,11 +33,29 @@ public class Main {
                 compressionFactory
         );
 
+        long initialSize = file.length();
+        System.out.println("Start processing file...\n");
+        System.out.printf("File: %s (%d bytes)\n", file.getName(), initialSize);
+        System.out.printf("Compression: %s\n", AppConfig.isCompressionEnabled());
+        System.out.printf("Algorithm: %s\n\n", AppConfig.getCompressionAlgorithm());
         Instant start = Instant.now();
         inMemoryService.processFile(file);
         Instant end = Instant.now();
 
         Duration timeElapsed = Duration.between(start, end);
         System.out.println("Time taken: " + timeElapsed.toMillis() + " ms");
+
+        Map<String, byte[]> chunks = inMemoryChunkRepository.getAllChunks();
+        int finalSize = chunks.values().stream().mapToInt(chunk -> chunk.length).sum();
+        double compressionPercentage = ((double) (initialSize - finalSize) / initialSize) * 100;
+        System.out.printf("Initial size: %d bytes, Final size: %d bytes, Compression: %.2f%%\n", initialSize, finalSize, compressionPercentage);
+    }
+
+    private static void printSplashScreen() {
+        System.out.println("==================================================");
+        System.out.println("=                                                =");
+        System.out.println("=               HETIC CDC Algorithm              =");
+        System.out.println("=                                                =");
+        System.out.println("==================================================");
     }
 }
